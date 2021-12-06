c1cde8750b360e71ef3e75230fc67eaf3efa2981
jeremyangles <80278261+jeremyangles@users.noreply.github.com>
2021-12-06 14:37:58 -0600

    Add files via upload

--- gameApi.php
+++ gameApi.php
@@ -37,17 +37,19 @@ switch ($type) {
    //debug_to_console("browse case");
        // Read the request type: game genre
        $genre = (isset($_GET['genre'])) ? $_GET['genre'] : '-1';
+       $pageNumber = (isset($_GET['page'])) ? $_GET['page'] : '-1';
+       $pageNumber = $pageNumber*25;
        //$rating = (isset($_GET['rating'])) ? $_GET['rating'] : '-1';
        if ($genre === '-1') {
           $response = $error;
        } else {
           if ($genre === 'All'){
               // Display a list of all the movies
-              $sql = "SELECT g.name, g.publisherName, r.averageRatings, g.price, g.genreName FROM Game g, Rating r where g.gameId=r.gameId order by name limit 50";
+              $sql = "SELECT g.name, g.publisherName, r.averageRatings, g.price, g.genreName FROM Game g, Rating r where g.gameId=r.gameId order by name LIMIT 25 OFFSET $pageNumber;";
           }
           else {
               // Display a list of movies based on the user selection
-              $sql = "SELECT /*g.gameId,*/ g.name, g.publisherName, r.averageRatings, g.price, g.genreName FROM Game g, Rating r where g.gameId=r.gameId AND genreName like '%$genre%' order by name limit 50";
+              $sql = "SELECT /*g.gameId,*/ g.name, g.publisherName, r.averageRatings, g.price, g.genreName FROM Game g, Rating r where g.gameId=r.gameId AND genreName like '%$genre%' order by name LIMIT 25 OFFSET $pageNumber;";
 
              // Define value(s) for 'named' parameters
              $parameterValues = array(':genre' => $genre);
@@ -66,29 +68,54 @@ switch ($type) {
          // Read the request type: game genre
 
       $search = (isset($_GET['search'])) ? $_GET['search'] : '-1';
-      //echo $rating;
-         if ($search === '-1') {
+      $pageNumber = (isset($_GET['page'])) ? $_GET['page'] : '-1';
+      $pageNumber = (int)$pageNumber*25;
+      $genre = (isset($_GET['genre'])) ? $_GET['genre'] : '-1';
+      $publisher = (isset($_GET['publisher'])) ? $_GET['publisher'] : '-1';
+      $rating = (isset($_GET['rating'])) ? $_GET['rating'] : '-1';
+      
+      if($search === 'null'){
+         $search = '';
+      }
+         
+      if($genre === 'null' || $genre === 'All'){
+         $genre = '';
+      }
+         
+      if($publisher === 'null' || $publisher === 'All'){
+         $publisher = '';
+      }
+         
+      if($rating === 'null' || $rating === 'All'){
+         $rating = '0';
+      }   
+      
+         if ($search === '-1' || $pageNumber === '-1' || $genre === '-1' || $publisher === '-1' || $rating === '-1') {
             $response = $error;
          } else {
-            if ($search === 'All' ){
-                // Display a list of all the movies
-                echo "NO PARAMETERS";
-                $sql = "SELECT g.name, g.publisherName, r.averageRatings, g.price, g.genreName FROM Game g, Rating r where g.gameId=r.gameId order by name limit 50";
-            } else {
-                echo "HAS PARAMETERS";
-                $sql = "SELECT g.name, g.publisherName, r.averageRatings, g.price, g.genreName FROM Game g, Rating r where g.gameId=r.gameId AND g.name LIKE '%$search%' order by name limit 50";
-               echo $sql;
-               // Define value(s) for 'named' parameters
-               //$parameterValues = array(':rating' => $rating);
+           //All values
+               $sql = "SELECT g.name, g.publisherName, r.averageRatings, g.price, g.genreName, g.gameId FROM Game g, Rating r where g.publisherName like '$publisher%' AND g.gameId=r.gameId AND g.name LIKE '%$search%' AND genreName like '%$genre%' AND r.averageRatings >= $rating order by name LIMIT 25 OFFSET $pageNumber;";
+               //echo $sql;
            }
-  
            // Execute SQL statement and obtain data
            $response = getAllRecords($sql, $db);
           //echo implode(" ", $response);
          
-        }
         
         break;
+        
+   case 'gameView':
+      $gameId = (isset($_GET['gameId'])) ? $_GET['gameId'] : '-1';
+      if ($gameId === '-1') {
+            $response = $error;
+         } else {
+           //All values
+               $sql = "SELECT g.name, g.publisherName, r.averageRatings, g.price, g.genreName, g.releaseDate, g.platformName, g.categoryName, g.description, r.totalNegative, r.totalPositive FROM Game g, Rating r where g.gameId=r.gameId and g.gameId=$gameId;";
+               //echo $sql;
+           }
+      $response = getAllRecords($sql, $db);
+      
+      break;
 
    default :
       $response = array();
@@ -107,34 +134,4 @@ function getAllRecords($sql, $db){
    $response = $stm->fetchAll(PDO::FETCH_ASSOC);
 return $response;
 }
-
-
-
-/*
-function getAllRecords($sql, $db, $values = null){
-   // prepare SQL statement
-   $stm = $db->prepare($sql);
-   // execute SQL statement
-   $stm->execute($values);
-   // fetch all records
-   $results = $stm->fetchAll(PDO::FETCH_ASSOC);
-   // return the result set
-   $tmpArr = array();
-   foreach ($results as $sub) {
-     $tmpArr[] = implode(',', $sub);
-   }
-   $result = implode('|', $tmpArr);
-   echo $result;   
-   return $results;
-
-   }
-*/
-/*
-function debug_to_console($data) {
-   $output = $data;
-   
-
-   echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
-*/
 ?>
---
+++ index.js
@@ -0,0 +1,137 @@
+displayNewGames();
+
+function previousPage(){
+  let params = new URLSearchParams(location.search);
+	var search = params.get('search');
+	var page = params.getAll('page');
+  const url = new URL(window.location);
+	let params1 = new URLSearchParams(url.search);
+	params1.set('search',search);
+  if(page > 0)
+	 params1.set('page',page - 1);
+  url.search = params1;
+	history.pushState({}, '', url);
+	document.getElementById('pageNumber').innerHTML = page;	
+  displayNewGames();
+}
+
+function nextPage(){
+  let params = new URLSearchParams(location.search);
+	var search = params.get('search');
+	var page = params.getAll('page');
+	page = parseInt(page);
+	console.log(page);
+	if(isNaN(page))
+		page = 0;
+  const url = new URL(window.location);
+	let params1 = new URLSearchParams(url.search);
+	page = page + 1;
+	params1.set('search',search);
+	params1.set('page',parseInt(page));
+  url.search = params1;
+  history.pushState({}, '', url);
+
+	document.getElementById('pageNumber').innerHTML = page;		
+  displayNewGames();
+}
+
+function changeFilter() {
+	let params = new URLSearchParams(location.search);
+	var search = params.get('search');
+	var genre = params.get('genre');
+	var publisher = params.get('publisher');
+	var rating = params.get('rating');
+	var page = 0;
+	document.getElementById('pageNumber').innerHTML = page;	
+	search = document.getElementById('search').value;
+	genre = document.getElementById('genre').value;
+	publisher = document.getElementById('publisher').value;
+	rating = document.getElementById('rating').value;
+	
+  	if(search != null)
+	 	document.getElementById('search').value = search;
+	if(genre != null)
+		document.getElementById('genre').value = genre;
+	if(publisher != null)
+  		document.getElementById('publisher').value = publisher;
+  	if(rating != null)
+  		document.getElementById('rating').value = rating
+  		
+	const url = new URL(window.location);
+	let params1 = new URLSearchParams(url.search);
+	params1.set('search',search);
+	params1.set('page',page);
+	params1.set('genre',genre);
+	params1.set('publisher',publisher);
+	params1.set('rating',rating);
+  	url.search = params1;
+	history.pushState({}, '', url);
+	
+  	displayNewGames();
+}
+
+function displayNewGames(){
+	console.log("Refreshing page");
+	let params = new URLSearchParams(location.search);
+	var search = params.get('search');
+	page = params.getAll('page');
+	var genre = params.get('genre');
+	var publisher = params.get('publisher');
+	var rating = params.get('rating');
+	
+	if(search === "")
+		search = null;
+	if(genre === "")
+		genre = null;
+	if(publisher == "")
+		publisher = null;
+	if(rating == "")
+		rating = null;
+		
+	console.log("Genre: " + genre);
+	console.log("Search: " + search);
+	console.log("Page: " + page);
+	console.log('http://localhost/whatshouldiplay2/gameApi.php?type=search&search='+search+'&page='+page + '&genre=' + genre + '&publisher=' + publisher + '&rating=' + rating);
+	getData('http://localhost/whatshouldiplay2/gameApi.php?type=search&search='+search+'&page='+page + '&genre=' + genre + '&publisher=' + publisher + '&rating=' + rating).then(function(response) {
+		console.log(response);
+		
+		const apiResponse = JSON.parse(response);
+		if (apiResponse.length > 0) {
+			const tRows = prepareHTMLContent(apiResponse);
+			document.getElementById('gamelist').innerHTML = tRows;
+			return true;
+		}
+		return false;
+	});
+	//console.log('apiResp: ' + apiResponse);
+}
+
+
+function getData(url) {
+	if (url) {
+		const task = new Promise( function(resolve, reject) {
+		const req = new XMLHttpRequest();
+		req.open('GET', url);
+		req.send();
+		req.onload = function(){
+			req.status === 200 ? resolve(req.response) : reject(Error(req.statusText));
+		}
+		req.onerror = function(e) { reject(Error(`Network Error: ${e}`));} 
+		});
+		return task;
+	}
+	return false;
+}
+
+function prepareHTMLContent(list) {
+	let output = "";
+	for(let i in list ) {
+	const games = list[i];
+		output += `<tr><td>${games.name}</td><td>${games.publisherName}</td><td>${games.averageRatings}</td><td>${games.price}</td><td>${games.genreName}</td><td><button onClick="gotoGameViewPage(this.id)" id=${games.gameId}>More Info</button></td></tr>`;
+	}
+	return output;
+}
+
+function gotoGameViewPage(clicked_id){
+	location.replace(location.origin + "/whatshouldiplay2/gameView.html?gameId=" + clicked_id);
+}
